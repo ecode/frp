ff0c6fbf8fd45139c03d632385ae7395edc1336f
bruce <bigtop@163.com>
2021-03-19 14:20:31 +0800

    add frps.sh

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Install
 wget https://raw.githubusercontent.com/ecode/frp/main/frpc.sh && chmod +x frpc.sh && ./frpc.sh
+wget https://raw.githubusercontent.com/ecode/frp/main/frps.sh && chmod +x frps.sh && ./frps.sh
--- frps.sh
+++ frps.sh
@@ -8,18 +8,18 @@ cd ${WORK_PATH} && \
 wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/frp_${FRP_VERSION}_linux_amd64.tar.gz && \
 tar -zxvf frp_${FRP_VERSION}_linux_amd64.tar.gz && \
 cd frp_${FRP_VERSION}_linux_amd64 && \
-mv frpc /usr/bin && \
-if [ ! -f "/etc/frp/frpc.ini" ]; then mv frpc.ini /etc/frp/; fi && \
-mv systemd/frpc.service /lib/systemd/system && \
+mv frps /usr/bin && \
+if [ ! -f "/etc/frp/frps.ini" ]; then mv frps.ini /etc/frp/; fi && \
+mv systemd/frps.service /lib/systemd/system && \
 systemctl daemon-reload && \
-sudo systemctl start frpc && \
-sudo systemctl enable frpc && \
+sudo systemctl start frps && \
+sudo systemctl enable frps && \
 cd ${WORK_PATH} && \
 rm -rf /tmp/frp
 
 echo "=======================================================" &&\
-echo -e "\033[32m安装成功,请先修改 frpc.ini 文件,确保格式及配置正确无误!\033[0m" && \
-echo -e "\033[31mvi /etc/frp/frpc.ini \033[0m" && \
+echo -e "\033[32m安装成功,请先修改 frps.ini 文件,确保格式及配置正确无误!\033[0m" && \
+echo -e "\033[31mvi /etc/frp/frps.ini \033[0m" && \
 echo -e "\033[32m修改完毕后执行以下命令重启服务:\033[0m" && \
-echo -e "\033[31msudo systemctl restart frpc\033[0m" && \
+echo -e "\033[31msudo systemctl restart frps\033[0m" && \
 echo "======================================================="
